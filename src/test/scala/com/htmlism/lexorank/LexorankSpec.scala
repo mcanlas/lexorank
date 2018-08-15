cfdf05daf8b5bc9f64c3f630f7bb2182ad5b901a
Mark Canlas <github@htmlism.com>
2018-08-15 12:54:40 -0400

    error mode for exists

--- src/test/scala/com/htmlism/lexorank/LexorankSpec.scala
+++ src/test/scala/com/htmlism/lexorank/LexorankSpec.scala
@@ -2,6 +2,12 @@ package com.htmlism.lexorank
 
 import org.scalatest._
 
-class LexorankSpec extends FreeSpec with Matchers {
-  it should ""
+class LexorankSpec extends FlatSpec with Matchers {
+  it should "error if pk exists in `after`" in {
+    Lexorank.changePosition(1, after = Some(1), before = None) shouldBe None
+  }
+
+  it should "error if pk exists in `before`" in {
+    Lexorank.changePosition(1, after = None, before = Some(1)) shouldBe None
+  }
 }
