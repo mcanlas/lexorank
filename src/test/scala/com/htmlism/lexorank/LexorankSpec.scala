3ce12d5f504a85ff79610591102e601859a089f0
Mark Canlas <github@htmlism.com>
2018-08-20 11:56:43 -0400

    redone as prop spec

--- src/test/scala/com/htmlism/lexorank/LexorankSpec.scala
+++ src/test/scala/com/htmlism/lexorank/LexorankSpec.scala
@@ -1,42 +1,44 @@
 package com.htmlism.lexorank
 
 import org.scalatest._
+import org.scalatest.prop._
 
-class LexorankSpec extends FlatSpec with Matchers {
+class LexorankSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
   it should "domain error if pk exists in `after`" in {
-    val key = 1
+    forAll { key: Int =>
+      val ret =
+        new Storage[String]
+          .changePosition(key, AfterBefore.after(key))
+          .value
+          .unsafeRunSync()
 
-    val ret =
-      new Storage[String]
-        .changePosition(key, AfterBefore.after(key))
-        .value
-        .unsafeRunSync()
-
-    ret shouldBe Left(IdWasInAfter)
+      ret shouldBe Left(IdWasInAfter)
+    }
   }
 
   it should "domain error if pk exists in `before`" in {
-    val key = 1
+    forAll { key: Int =>
+      val ret =
+        new Storage[String]
+          .changePosition(key, AfterBefore.before(key))
+          .value
+          .unsafeRunSync()
 
-    val ret =
-      new Storage[String]
-        .changePosition(key, AfterBefore.before(key))
-        .value
-        .unsafeRunSync()
-
-    ret shouldBe Left(IdWasInBefore)
+      ret shouldBe Left(IdWasInBefore)
+    }
   }
 
   it should "domain error if pk does not exist" in {
-    val pk = 1
-    val pos = 2
+    forAll { (keyA: Int, keyB: Int) =>
+      whenever (keyA != keyB) {
+        val ret =
+          new Storage[String]
+            .changePosition(keyA, AfterBefore.after(keyB))
+            .value
+            .unsafeRunSync()
 
-    val ret =
-      new Storage[String]
-        .changePosition(pk, AfterBefore.after(pos))
-        .value
-        .unsafeRunSync()
-
-    ret shouldBe Left(IdDoesNotExistInStorage)
+        ret shouldBe Left(IdDoesNotExistInStorage)
+      }
+    }
   }
 }
