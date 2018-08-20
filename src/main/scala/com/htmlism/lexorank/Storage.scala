0ce047472f30094770a0c2fd3cb3a671661cdc5c
Mark Canlas <github@htmlism.com>
2018-08-20 16:50:12 -0400

    with row as builder

--- src/main/scala/com/htmlism/lexorank/Storage.scala
+++ src/main/scala/com/htmlism/lexorank/Storage.scala
@@ -1,8 +1,5 @@
 package com.htmlism.lexorank
 
-import cats.effect._
-import mouse.all._
-
 /**
  * We can consciously choose not to support the use case of inserting new records in storage that currently has no
  * records in it. The existing `changePosition` method assumes that there is something that exists prior that needs
@@ -37,15 +34,15 @@ class Storage[K, A] {
   def getSnapshot: AnnotatedIO[Snapshot] =
     AnnotatedIO(xs.map(r => r.id -> r.x).toMap)
 
-  def withRow(id: K, rank: A): IO[Row] =
-    IO {
-      val ret = Entity(id, rank)
+  def withRow(id: K, rank: A): this.type =
+    {
+      val row = Entity(id, rank)
 
-      xs += ret
+      xs += row
 
       assert(xs.map(_.id).toSet.size == xs.size, "primary keys are unique")
       assert(xs.map(_.x).toSet.size == xs.size, "ranks are unique")
 
-      ret
+      this
     }
 }
