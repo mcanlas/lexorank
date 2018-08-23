d6cd9476b8cb2f0d534a67350cd36743c2470cef
Mark Canlas <github@htmlism.com>
2018-08-23 17:24:15 -0400

    added mind types notice

--- src/main/scala/com/htmlism/lexorank/Storage.scala
+++ src/main/scala/com/htmlism/lexorank/Storage.scala
@@ -99,6 +99,14 @@ class Storage[K, R](rankGenerator: RankGenerator[R])(implicit K: KeyLike[K], R: 
         Right(id -> Record("", RG.anywhere))
       }
 
+  /**
+   * This, in your mind types, opens a connection to the database. It uses a "select for update" lock to hold to
+   * hold on to the entire key space, since the worst case scenario is that many of them get updated.
+   *
+   * Remember that your key space may also be a composite of two columns. If your rank column is `Int` and there is
+   * an "organization" column that you also key by, this only needs to lock on one organization's ranks (if your
+   * operations are always per-organization and never enforce global ranking across all organizations).
+   */
   private def getSnapshot: AnnotatedIO[Snapshot] =
     AnnotatedIO(xs.map(r => r._1 -> r._2.rank).toMap)
 
