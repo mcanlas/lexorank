98d4e7e7ef7c026ad453e9a594db94976521bb5b
Mark Canlas <github@htmlism.com>
2018-08-23 16:24:21 -0400

    added commentary

--- src/main/scala/com/htmlism/lexorank/RangedGenerator.scala
+++ src/main/scala/com/htmlism/lexorank/RangedGenerator.scala
@@ -1,6 +1,9 @@
 package com.htmlism.lexorank
 
 /**
+ * A strategy for generating values between `min` and `max`. It could be something like a random number or the
+ * midpoint.
+ *
  * Isolates non-determinism.
  */
 trait RangedGenerator[A] {
