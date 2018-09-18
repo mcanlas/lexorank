6c35666d0f3b06a5dd41d78fd5dec9995aa651ef
Mark Canlas <github@htmlism.com>
2018-09-18 12:01:45 -0400

    add type aliases

--- src/main/scala/com/htmlism/lexorank/Storage.scala
+++ src/main/scala/com/htmlism/lexorank/Storage.scala
@@ -7,4 +7,16 @@ package com.htmlism.lexorank
  * @tparam K The type for primary keys in this storage. Usually `Int`
  * @tparam R The type for ranking items relative to one another. Usually `Int` but could be something like `String`
  */
-trait Storage[F[_], K, R]
+trait Storage[F[_], K, R] {
+  /**
+   * Conceptually a row in a relational database, containing a primary, a payload, and a rank.
+   */
+  type Row = (K, Record[R])
+
+  /**
+   * Easy access for relating unique keys to unique ranks. This is conceptually a bidirectional map.
+   *
+   * In SQL, this could be backed by a composite index over PK and ranks and enable index scans.
+   */
+  type Snapshot = Map[K, R]
+}
