467eb66449dba641ef632bd13004b99e284647e3
Mark Canlas <github@htmlism.com>
2018-09-18 11:12:28 -0400

    add stubs for collection guts

--- src/main/scala/com/htmlism/lexorank/ScalaCollectionStorage.scala
+++ src/main/scala/com/htmlism/lexorank/ScalaCollectionStorage.scala
@@ -1,3 +1,16 @@
 package com.htmlism.lexorank
 
-trait ScalaCollectionStorage[K, R] extends Storage[K, R]
+/**
+ * @param K Evidence for key behaviors over `K`
+ * @param R Evidence for rank behaviors over `R`
+ */
+class ScalaCollectionStorage[K, R](implicit K: KeyLike[K], R: Rankable[R]) extends Storage[K, R] {
+  private var pkSeed: K =
+    K.first
+
+  /**
+   * This is bi-directional map between PKs and ranks.
+   */
+  private val xs =
+    collection.mutable.Map.empty[K, Record[R]]
+}
