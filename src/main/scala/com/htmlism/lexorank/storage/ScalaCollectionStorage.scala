f45f60dc2947ccd9acc75f4a46294a96c96e7949
Mark Canlas <github@htmlism.com>
2018-09-19 14:56:50 -0400

    enable state injection for storage

--- src/main/scala/com/htmlism/lexorank/storage/ScalaCollectionStorage.scala
+++ src/main/scala/com/htmlism/lexorank/storage/ScalaCollectionStorage.scala
@@ -4,22 +4,43 @@ package storage
 import cats.implicits._
 import cats.effect._
 
+object ScalaCollectionStorage {
+  /**
+   * Factory from nothing.
+   *
+   * @tparam F An effect type
+   * @tparam K A key type
+   * @tparam R A rank type
+   */
+  def empty[F[_] : Sync, K : KeyLike, R]: ScalaCollectionStorage[F, K, R] =
+    from(Map.empty)
+
+  /**
+   * Factory method for seeding state
+   *
+   * @param xs An immutable bi-directional map. Construction will bomb if the map is not bidirectionally unique
+   *
+   * @tparam F An effect type
+   * @tparam K A key type
+   * @tparam R A rank type
+   */
+  def from[F[_] : Sync, K : KeyLike, R](xs: Map[K, Record[R]]): ScalaCollectionStorage[F, K, R] =
+    new ScalaCollectionStorage(collection.mutable.Map(xs.toList: _*))
+}
+
 /**
+ * @param xs A bi-directional map between PKs and ranks.
  * @param K Evidence for key behaviors over `K`
  *
  * @tparam F An effect type
  * @tparam K The type for primary keys in this storage. Usually `Int`
  * @tparam R The type for ranking items relative to one another. Usually `Int` but could be something like `String`
  */
-class ScalaCollectionStorage[F[_], K, R](implicit F: Sync[F], K: KeyLike[K]) extends Storage[F, K, R] {
+class ScalaCollectionStorage[F[_], K, R](xs: collection.mutable.Map[K, Record[R]])(implicit F: Sync[F], K: KeyLike[K]) extends Storage[F, K, R] {
+  assertUniqueRanks()
+
   private var pkSeed: K =
     K.first
-
-  /**
-   * This is bi-directional map between PKs and ranks.
-   */
-  private val xs =
-    collection.mutable.Map.empty[K, Record[R]]
 
   def lockSnapshot: F[Snapshot] =
     F.delay {
