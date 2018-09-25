8c531ed9f5e829dd573fed8943a7989dc7fae782
Mark Canlas <github@htmlism.com>
2018-09-25 17:24:09 -0400

    between tests

---
+++ src/test/scala/com/htmlism/lexorank/BetweenSpec.scala
@@ -0,0 +1,32 @@
+package com.htmlism.lexorank
+
+import cats.instances.int._
+
+import org.scalatest._
+import org.scalatest.prop._
+
+class BetweenSpec
+    extends FlatSpec
+    with Matchers
+    with Inside
+    with OptionValues
+    with EitherValues
+    with GeneratorDrivenPropertyChecks {
+  "valid pairs" should "construct properly" in {
+    forAll { (x: Int, y: Int) =>
+      whenever(x != y) {
+        inside(Between(x, y)) {
+          case Right(ab) =>
+            ab.after.value shouldBe x
+            ab.before.value shouldBe y
+        }
+      }
+    }
+  }
+
+  "invalid pairs" should "construct properly" in {
+    forAll { x: Int =>
+      Between(x, x).left.value shouldBe errors.DuplicateBetweenKeys
+    }
+  }
+}
