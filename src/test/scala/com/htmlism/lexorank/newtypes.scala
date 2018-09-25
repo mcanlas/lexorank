c1990d2b54f609bf278e03e219eca0d715625d31
Mark Canlas <github@htmlism.com>
2018-09-25 15:50:22 -0400

    clarify as store

--- src/test/scala/com/htmlism/lexorank/newtypes.scala
+++ src/test/scala/com/htmlism/lexorank/newtypes.scala
@@ -6,15 +6,15 @@ package com.htmlism.lexorank
   *
   * This is a newtype to differentiate between two completely arbitrary/random instances.
   *
-  * @param flow A Lexorank instance
+  * @param store Lexorank storage
   * @param req A position request for that instance
   *
-  * @tparam F A monadic effect type
+  * @tparam F A monadic effect type for storage
   * @tparam K A key type
   * @tparam R A rank type
   */
 case class StorageAndValidInsertRequest[F[_], K, R, PR[_] <: PositionRequest[_]](
-    flow: storage.ScalaCollectionStorage[F, K, R],
+    store: storage.ScalaCollectionStorage[F, K, R],
     req: PR[K])
 
 /**
@@ -23,15 +23,15 @@ case class StorageAndValidInsertRequest[F[_], K, R, PR[_] <: PositionRequest[_]]
   *
   * This is a newtype to differentiate between two completely arbitrary/random instances.
   *
-  * @param flow A Lexorank instance
+  * @param store Lexorank storage
   * @param pk The identifier for the row being changed
   * @param req A position request for that instance
   *
-  * @tparam F A monadic effect type
+  * @tparam F A monadic effect type for storage
   * @tparam K A key type
   * @tparam R A rank type
   */
 case class StorageAndValidChangeRequest[F[_], K, R, PR[_] <: PositionRequest[_]](
-    flow: storage.ScalaCollectionStorage[F, K, R],
+    store: storage.ScalaCollectionStorage[F, K, R],
     pk: K,
     req: PR[K])
