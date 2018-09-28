c397d57160a75aad341f29c8989c1430d2f4abd8
Mark Canlas <github@htmlism.com>
2018-09-28 17:10:43 -0400

    drop unused

--- src/main/scala/com/htmlism/lexorank/ChangeError.scala
+++
@@ -1,9 +0,0 @@
-package com.htmlism.lexorank
-
-sealed trait ChangeError
-
-case object IdWasInBefore extends ChangeError
-
-case object IdWasInAfter extends ChangeError
-
-case object IdDoesNotExistInStorage extends ChangeError
