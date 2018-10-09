f5fdf40f98cb0c34a96c8085909b6dda4d6e49ce
Mark Canlas <github@htmlism.com>
2018-10-09 12:00:24 -0400

    as monad

--- src/main/scala/com/htmlism/lexorank/Storage.scala
+++ src/main/scala/com/htmlism/lexorank/Storage.scala
@@ -3,7 +3,7 @@ package com.htmlism.lexorank
 /**
   * Storage interface, backed by a Scala collection or SQL engine (like H2).
   *
-  * @tparam F An effect type
+  * @tparam F A monadic effect type
   * @tparam K The type for primary keys in this storage. Usually `Int`
   * @tparam R The type for ranking items relative to one another. Usually `Int` but could be something like `String`
   */
