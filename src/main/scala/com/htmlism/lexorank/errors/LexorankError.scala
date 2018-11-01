d71649ebdc0bd2b51442c2d00c6dbd9fc1584620
Mark Canlas <github@htmlism.com>
2018-11-01 15:05:35 -0400

    add scaladoc for errors

--- src/main/scala/com/htmlism/lexorank/errors/LexorankError.scala
+++ src/main/scala/com/htmlism/lexorank/errors/LexorankError.scala
@@ -5,20 +5,52 @@ package com.htmlism.lexorank.errors
   */
 sealed trait LexorankError
 
+/**
+  * An input validation error that occurs before any IO occurs.
+  */
 sealed trait RequestError extends LexorankError
+
+/**
+  * An error that occurs after some IO occurs.
+  */
 sealed trait AfterIoError extends LexorankError
 
-case object DuplicateChangeKeys  extends RequestError
-case object DuplicateBetweenKeys extends RequestError
+/**
+  * The key of a change request cannot be the among the keys of the underlying position request.
+  */
+object DuplicateChangeKeys extends RequestError
+
+/**
+  * The two keys of a between request cannot be the same.
+  */
+object DuplicateBetweenKeys extends RequestError
 
 /**
   * When a requested row key or position request key does not exist in the context snapshot.
   */
 case object KeyNotInContext extends AfterIoError
-sealed trait OverflowError  extends AfterIoError
 
+/**
+  * An attempt to adjust a rank wraps around an end of a finite key space, violating order.
+  */
+sealed trait OverflowError extends AfterIoError
+
+/**
+  * When incrementing a rank wraps around the maximum of a key space.
+  */
 sealed trait MaxOverflow extends OverflowError
-case object MaxOverflow  extends MaxOverflow
 
+/**
+  * Singleton inhabitant.
+  */
+object MaxOverflow extends MaxOverflow
+
+/**
+  * When decrementing a rank wraps around the minimum of a key space.
+  */
 sealed trait MinUnderflow extends OverflowError
-case object MinUnderflow  extends MinUnderflow
+
+/**
+  * Singleton inhabitant.
+  */
+object MinUnderflow extends MinUnderflow
