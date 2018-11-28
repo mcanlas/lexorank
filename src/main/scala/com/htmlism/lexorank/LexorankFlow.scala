4f8b28d3f61301b5bb8ccb194e92c05627519064
Mark Canlas <github@htmlism.com>
2018-11-28 15:32:08 -0500

    Add PlantUML sequence diagram (#16)

--- src/main/scala/com/htmlism/lexorank/LexorankFlow.scala
+++ src/main/scala/com/htmlism/lexorank/LexorankFlow.scala
@@ -52,6 +52,7 @@ class LexorankFlow[F[_], G[_]: Monad, K, R](tx: G ~> F, store: Storage[G, K, R],
   /**
     * Scala `ListSet` is buggy in 2.11 so don't bother.
     */
+  // TODO it is possible to push the sorting into the storage contract
   def getRows(implicit ord: Ordering[R]): F[List[K]] =
     tx {
       store.getSnapshot
