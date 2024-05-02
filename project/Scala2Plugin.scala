e0ad3d1398ab284868e15120ce2b443bf3e0dcaf
Mark Canlas <github@htmlism.com>
2024-05-02 13:05:28 -0400

    scala 2.13.14

--- project/Scala2Plugin.scala
+++ project/Scala2Plugin.scala
@@ -12,7 +12,7 @@ object Scala2Plugin extends AutoPlugin {
   override def trigger: PluginTrigger = AllRequirements
 
   override val buildSettings: Seq[Setting[?]] = Seq(
-    scalaVersion := "2.13.13"
+    scalaVersion := "2.13.14"
   )
 
   // putting into project settings helps with late binding for properly detecting scala3 cross build?
